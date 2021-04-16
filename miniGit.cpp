364eaf6e22c645ccb1a20cf2b2813a9aa9a5c719
collinsinclair <collin.sinclair@colorado.edu>
2021-04-16 01:16:00 -0600

    updated miniGit::add() and miniGit::remove() to prompt user for filename; added TODOs

--- miniGit.cpp
+++ miniGit.cpp
@@ -4,33 +4,37 @@
 
 #include "miniGit.h"
 miniGit::miniGit() {}
-miniGit::~miniGit() {
-
-}
+miniGit::~miniGit() {}
 void miniGit::add() {
   // prompt user to enter a file name
+  cout << "Enter a file name: ";
+  string filename;
+  getline(cin, filename);
 
-  // check whether the file with the given name exists in the current directory.
-  // If not, keep prompting the user to enter a valid file name.
+  // TODO check whether the file with the given name exists in the current directory.
+  // TODO If not, keep prompting the user to enter a valid file name.
 
-  // The SLL is checked to see whether the file has already been added.
+  // TODO The SLL is checked to see whether the file has already been added.
   // A file by the same name cannot be added twice.
 
-  // A new SLL node gets added containing the name of the input file, name of the repository file,
+  // TODO A new SLL node gets added containing the name of the input file, name of the repository file,
   // as well as a pointer to the next node.
   // The repository file name should be the combination of the original file name and the version number.
 }
 void miniGit::remove() {
   // Prompt the user to enter a file name.
+  cout << "Enter a file name: ";
+  string filename;
+  getline(cin, filename);
 
-  // Check the SLL for whether the file exists in the current version of the repository.
+  // TODO Check the SLL for whether the file exists in the current version of the repository.
 
-  // If found, delete the SLL node.
+  // TODO If found, delete the SLL node.
 }
 void miniGit::commit() {
-  // The current SLL should be traversed in its entirety, and for every node: see writeup
+  // TODO The current SLL should be traversed in its entirety, and for every node: see writeup
 
-  // Once all the files have been scanned, the final step of the commit will create a new DLL node of the repo
+  // TODO Once all the files have been scanned, the final step of the commit will create a new DLL node of the repo
 
 }
 void miniGit::checkout() {
